aed993c03c977eab9c70fe2a25fe098ab3fead5c
Varun Iyer <varun_iyer@posteo.net>
2019-11-05 04:54:52 -0800

    Draw_background()

--- main.c
+++ main.c
@@ -85,10 +85,30 @@ u32 setup() {
 	 
 }
 
+ 
+#define LCD_WIDTH 320
+#define LCD_HEIGHT 240
+#define setPix(x, y) fillRect(x, y, x, y)
+void draw_background () {
+	u32 pixs = 0x333333
+	for(int x = 0; x < LCD_WIDTH; x++) {
+		for(int y = 0; y < LCD_HEIGHT; y++) {
+			setColor((pixs >> 16) & 0xFF, (pixs >> 8) & 0xFF, pixs & 0xFF);
+			setPix(x, y)
+		}
+	}
 
+	setColor(255, 255, 255);
+	for(int y = 10; y + 30 < LCD_WIDTH; y += 40) {
+		for(int x = 10; x + 30 < LCD_WIDTH; x += 40) {
+			fillRect(x, y, x + 30; y + 30);
+		}
+	}
+}
+
+ 
 int main()
 {
-
 	int sec = 0;
 	int secTmp;
 	char secStr[4];
@@ -97,38 +117,6 @@ int main()
 	 
 	clrScr();
 
-	setColor(255, 0, 0);
-	fillRect(20, 20, 220, 300);
-
-	setColor(0, 50, 100);
-	fillRect(40, 180, 200, 250);
-
-	setColor(0, 255, 0);
-	setColorBg(255, 0, 0);
-	lcdPrint("Hello !!!!!", 40, 60);
-
-	setFont(BigFont);
-	lcdPrint("<# WORLD #>", 40, 80);
-
-	setFont(SevenSegNumFont);
-	setColor(238, 64, 0);
-	setColorBg(0, 50, 100);
-	while (1) {
-		if (timerTrigger > 0) {
-			secTmp = sec++ % 1000;
-			secStr[0] = secTmp / 100 + 48;
-			secTmp -= (secStr[0] - 48) * 100;
-			secStr[1] = secTmp / 10 + 48;
-			secTmp -= (secStr[1] - 48) * 10;
-			secStr[2] = secTmp + 48;
-			secStr[3] = '\0';
-
-			lcdPrint(secStr, 70, 190);
-
-			timerTrigger = 0;
-		}
-	}
-
-	xil_printf("End\n");
+	draw_background();
 	return 0;
 }
